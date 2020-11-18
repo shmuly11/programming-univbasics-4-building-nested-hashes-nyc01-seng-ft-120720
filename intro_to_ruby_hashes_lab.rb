0dc94a31637c3875d803f37fff6f2f3e6f927c29
shmuly levitin <shmuly11@gmail.com>
2020-11-18 15:34:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
-      :shortline_ralroad => {}
+      :shortline_railroad => {}
     }
   }
     
