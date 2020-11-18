864fcfab0530e5ed0210c43e2e0ba9bef90310ec
shmuly levitin <shmuly11@gmail.com>
2020-11-18 15:27:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # complexity.
   base_hash = {:railroads => {
     :pieces => "4",
-    :rent_in_dollars => {}
+    :rent_in_dollars => {},
     :names => {}
   }
     
